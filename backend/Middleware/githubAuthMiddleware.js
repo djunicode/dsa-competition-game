5f8e65bfafada59d66d5687dd419c3089c1af292
Meet <meetp272003@gmail.com>
2022-04-03 20:18:33 +0530

    Signin through Github

---
+++ backend/Middleware/githubAuthMiddleware.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const passport = require('passport');
+
+const githubAuth = passport.authenticate('github', { failureRedirect: '/api/user/githubFailed' });
+
+
+module.exports = {
+    githubAuth
+}
